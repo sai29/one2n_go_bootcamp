afe061dc9b8b6c31c09c5a6277ca84f0af40c2eb
Sai Prashanth <saiprashanth93@gmail.com>
2024-06-24 18:08:32 +0530

    Word count - first implementation(without tests)

--- wc/main.go
+++ wc/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Word counting done here!")
+	os.Exit(run())
+}
+
+func run() int {
+	flag.Bool("l", false, "a bool")
+	flag.Parse()
+	errorCode := 0
+
+	if isFlagPassed("l") {
+		filePath := flag.Args()[0]
+		errorCode = readLines(filePath)
+	}
+	// fmt.Println("error code is\n", errorCode)
+	return errorCode
+}
+
+func readLines(filePath string) int {
+	file, err := os.Open(filePath)
+	lineCount := 0
+
+	if err != nil {
+		fmt.Println(err)
+		return 126
+		// log.Fatal("Unable to read input file or permission issue with file"+filePath, err)
+	} else {
+		info, _ := os.Stat(filePath)
+		if info.IsDir() {
+			fmt.Printf("wc: %s: Is a directory\n", filePath)
+			return 126
+		}
+	}
+
+	defer file.Close()
+	reader := bufio.NewReader(file)
+
+	for {
+		_, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		} else {
+			lineCount += 1
+		}
+
+	}
+	// fmt.Println(lineCount, filePath)
+	fmt.Printf("%8v %8v\n", lineCount, filePath)
+	return 0
+}
+
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
 }
