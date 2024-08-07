7e112b4a822b768858ee4a35c52c2df2991cf8ad
Sai Prashanth <saiprashanth93@gmail.com>
2024-08-07 14:54:25 +0530

    Add Cobra and prints args

--- grep/main.go
+++ grep/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("Here we grep")
+	if err := rootCmd.Execute(); err != nil {
+		printToStderr(err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "grep",
+	Short: "grep is used to find the presence of an input string",
+	Long:  "grep is given an input of a STDIN/file/directory and will confirm the presence of an input string if it is present in the entity we are checking on.",
+	Run: func(cmd *cobra.Command, args []string) {
+		printToStdOut(args[0])
+		fmt.Println("\n")
+	},
+}
+
+func printToStderr(err error) {
+	fmt.Fprint(os.Stderr, err)
+}
+
+func printToStdOut(str string) {
+	fmt.Fprint(os.Stdout, str)
 }
