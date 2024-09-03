dd90eff9bc0d12b231b6c4455b2e3c0dbc7fc4fc
Sai Prashanth <saiprashanth93@gmail.com>
2024-09-03 14:01:51 +0530

    Cobra boilerplate

--- tree/main.go
+++ tree/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("Hello tree!")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "tree",
+	Short: "Print file directory structure in the form of a tree at given input directory",
+	Long:  "Print the directory structure at the given directory with various flags to modify the output",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello tree!")
+	},
 }
